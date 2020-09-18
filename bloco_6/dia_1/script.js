64fd7bf5db0ddb5536edfeb3e7f39c42ba3b0f77
Lfocosi <luccafocosi@gmail.com>
2020-09-18 13:53:51 -0300

    me perdi nos commits

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -1,5 +1,8 @@
 let estados = ["Acre (AC)", "Alagoas (AL)", "Amapá (AP)", "Amazonas (AM)", "Bahia (BA)", "Ceará (CE)", "Distrito Federal (DF)", "Espírito Santo (ES)", "Goiás (GO)", "Maranhão (MA)", "Mato Grosso (MT)", "Mato Grosso do Sul (MS)", "Minas Gerais (MG)", "Pará (PA)", "Paraíba (PB)", "Paraná (PR)", "Pernambuco (PE)", "Piauí (PI)", "Rio de Janeiro (RJ)", "Rio Grande do Norte (RN)", "Rio Grande do Sul (RS)", "Rondônia (RO)", "Roraima (RR)", "Santa Catarina (SC)", "São Paulo (SP)", "Sergipe (SE)", "Tocantins (TO)"];
 let locestados = document.getElementById('Estado');
+let locdia = document.getElementById('Dia');
+let locmes = document.getElementById('Mes');
+let locano = document.getElementById('Ano');
 function criando() {
   for (let numero = 0; numero < estados.length; numero += 1) {
     let option = document.createElement("option");
@@ -9,4 +12,36 @@ function criando() {
   }
 }
 
-window.onload = criando;
+function criandoD() {
+  for (let dia = 01; dia <= 31; dia += 1) {
+    let optd = document.createElement("option");
+    optd.value = dia;
+    optd.innerText = dia;
+    locdia.appendChild(optd);
+  }
+}
+
+function criandoM() {
+  for (let mes = 01; mes <= 12; mes += 1) {
+    let optm = document.createElement("option");
+    optm.value = mes;
+    optm.innerText = mes;
+    locmes.appendChild(optm);
+  }
+}
+
+function criandoA() {
+  for (let ano = 2020; ano >= 1600; ano -= 1) {
+    let opta = document.createElement("option");
+    opta.value = ano;
+    opta.innerText = ano;
+    locano.appendChild(opta);
+  }
+}
+
+window.onload = function () {
+  criando();
+  criandoA();
+  criandoD();
+  criandoM();
+};
